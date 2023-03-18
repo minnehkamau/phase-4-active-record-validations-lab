aac8c8663eedcd5555da2e0d619fe0c0dae95ceb
minnehkamau <minnieches08@gmail.com>
2023-03-18 16:55:43 +0300

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait_title
+  private
+
+  def clickbait_title
+    if title.present? && !title.include?("You Won't Believe")
+      errors.add(:title, 'must be clickbait')
+    end
+  end
 end
